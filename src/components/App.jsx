7c97aff8defcb7692f8c002165b5095408b7db69
Aryan <aryan.saxena094@gmail.com>
2023-03-30 03:08:21 -0400

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import Form from "./Login";
+
+var isLoggedIn = false;
+
+function renderConditional() {
+if (isLoggedIn===true){
+  return <h1>Hello</h1>;
+} else {
+  return <Form />;
+}
+}
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {renderConditional()}
     </div>
   );
 }
