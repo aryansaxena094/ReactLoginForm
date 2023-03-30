027f98ecc83c9d52a29aba9d72200fd62c279c5a
Aryan <aryan.saxena094@gmail.com>
2023-03-30 03:40:33 -0400

    refactor

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,6 +6,7 @@ function Login() {
         <form className="form">
             <Input type = "text" placeholder="Username" />
             <Input type = "password" placeholder="Password" />
+            <Input type = "password" placeholder="Confirm Password" />
             <button type="submit">Login</button>
         </form>
         );
